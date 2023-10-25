529c5338ce6628f17d57d54117caa4bfc858e1e4
Sharon C <99379001+schris48@users.noreply.github.com>
2023-10-25 14:41:02 -0400

    Update audio-settings.js

--- audio-settings.js
+++ audio-settings.js
@@ -1,47 +1,45 @@
 videojs.registerPlugin('customSettingsMenu', function() {
-    var player = this;
-  
-    // Create a div for the settings menu
-    var settingsMenu = document.createElement("div");
-    settingsMenu.className = "custom-settings-menu";
-  
-    // Create a cog icon for the settings menu
-    var cogIcon = document.createElement("div");
-    cogIcon.className = "cog-icon";
-    cogIcon.innerHTML = "&#x2699;"; // Unicode for cog icon
-  
-    // Create a div for the CC and DV buttons
-    var ccDvButtons = document.createElement("div");
-    ccDvButtons.className = "cc-dv-buttons";
-  
-    // Create the CC button
-    var ccButton = document.createElement("button");
-    ccButton.className = "vjs-control vjs-button vjs-closed-caption-button";
-    ccButton.innerHTML = "<span class='vjs-icon-placeholder' aria-hidden='true'></span><span class='vjs-control-text' aria-live='polite'>CC</span>";
-    
-    // Create the DV button
-    var dvButton = document.createElement("button");
-    dvButton.className = "vjs-control vjs-button vjs-display-video-button";
-    dvButton.innerHTML = "<span class='vjs-icon-placeholder' aria-hidden='true'></span><span class='vjs-control-text' aria-live='polite'>DV</span>";
-  
-    // Append the CC and DV buttons to the ccDvButtons div
-    ccDvButtons.appendChild(ccButton);
-    ccDvButtons.appendChild(dvButton);
-  
-    // Append the cog icon and ccDvButtons to the settingsMenu
-    settingsMenu.appendChild(cogIcon);
-    settingsMenu.appendChild(ccDvButtons);
-  
-    // Append the settingsMenu to the control bar
-    var controlBar = player.controlBar.el();
-    controlBar.appendChild(settingsMenu);
-  
-    // Add click event listener for the cog icon to toggle the settings menu
-    cogIcon.addEventListener("click", function() {
-      if (ccDvButtons.style.display === "block") {
-        ccDvButtons.style.display = "none";
-      } else {
-        ccDvButtons.style.display = "block";
-      }
-    });
-  });  
+  var player = this;
+
+  // Create a div for the settings menu
+  var settingsMenu = document.createElement("div");
+  settingsMenu.className = "custom-settings-menu";
+
+  // Create a cog icon for the settings menu
+  var cogIcon = document.createElement("div");
+  cogIcon.className = "cog-icon";
+  cogIcon.innerHTML = "&#x2699"; // Unicode for cog icon
+
+  // Create a div for the CC and Audio Track controls
+  var ccAudioControls = document.createElement("div");
+  ccAudioControls.className = "cc-audio-controls";
+
+  // Create the CC button
+  var ccButton = document.querySelector('.vjs-subs-caps-button');
+  ccButton.className = "vjs-control vjs-button vjs-settings-control"; // Change class to vjs-settings-control
+
+  // Create the Audio Track button
+  var audioButton = document.querySelector('.vjs-audio-button');
+  audioButton.className = "vjs-control vjs-button vjs-settings-control"; // Change class to vjs-settings-control
+
+  // Append the CC and Audio Track buttons to the ccAudioControls div
+  ccAudioControls.appendChild(ccButton);
+  ccAudioControls.appendChild(audioButton);
+
+  // Append the cog icon and ccAudioControls to the settingsMenu
+  settingsMenu.appendChild(cogIcon);
+  settingsMenu.appendChild(ccAudioControls);
+
+  // Append the settingsMenu to the control bar
+  var controlBar = player.controlBar.el();
+  controlBar.appendChild(settingsMenu);
+
+  // Add click event listener for the cog icon to toggle the settings menu
+  cogIcon.addEventListener("click", function() {
+    if (ccAudioControls.style.display === "block") {
+      ccAudioControls.style.display = "none";
+    } else {
+      ccAudioControls.style.display = "block";
+    }
+  });
+});
