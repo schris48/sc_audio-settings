94e2e92a905aca5f41d4bfffac4418ab0e88ff77
Sharon C <99379001+schris48@users.noreply.github.com>
2023-10-25 15:02:36 -0400

    Update audio-settings.js

--- audio-settings.js
+++ audio-settings.js
@@ -1,45 +1,66 @@
-videojs.registerPlugin('customSettingsMenu', function() {
+// Wait for the Brightcove player to be ready
+videojs.getPlayer('JQrljZgnO6').ready(function () {
   var player = this;
 
-  // Create a div for the settings menu
-  var settingsMenu = document.createElement("div");
-  settingsMenu.className = "custom-settings-menu";
+  // Create a new Settings button
+  var settingsButton = document.createElement('button');
+  settingsButton.className = 'vjs-settings-button vjs-menu-button vjs-menu-button-popup vjs-control vjs-button';
+  settingsButton.type = 'button';
+  settingsButton.innerHTML = '<span class="vjs-icon-placeholder" aria-hidden="true"></span><span class="vjs-control-text" aria-live="polite">Settings</span>';
+  
+  // Create a new Settings menu
+  var settingsMenu = document.createElement('div');
+  settingsMenu.className = 'vjs-menu';
+  
+  // Create and add items to the Settings menu
+  var captionsSettingsItem = document.createElement('li');
+  captionsSettingsItem.className = 'vjs-menu-item vjs-texttrack-settings';
+  captionsSettingsItem.innerHTML = '<span class="vjs-menu-item-text">captions settings</span><span class="vjs-control-text" aria-live="polite">, opens captions settings dialog</span>';
+  
+  var audioSettingsItem = document.createElement('li');
+  audioSettingsItem.className = 'vjs-menu-item vjs-texttrack-settings';
+  audioSettingsItem.innerHTML = '<span class="vjs-menu-item-text">audio settings</span><span class="vjs-control-text" aria-live="polite">, opens audio settings dialog</span>';
+  
+  settingsMenu.appendChild(captionsSettingsItem);
+  settingsMenu.appendChild(audioSettingsItem);
+  
+  // Add the Settings button and menu to the control bar
+  var controlBar = player.getChild('ControlBar');
+  controlBar.addChild(settingsButton);
+  controlBar.el().appendChild(settingsMenu);
 
-  // Create a cog icon for the settings menu
-  var cogIcon = document.createElement("div");
-  cogIcon.className = "cog-icon";
-  cogIcon.innerHTML = "&#x2699"; // Unicode for cog icon
+  // Hide the original Captions and Audio Track buttons
+  var captionsButton = player.controlBar.getChild('ClosedCaptionButton');
+  var audioButton = player.controlBar.getChild('AudioTrackButton');
+  captionsButton.hide();
+  audioButton.hide();
 
-  // Create a div for the CC and Audio Track controls
-  var ccAudioControls = document.createElement("div");
-  ccAudioControls.className = "cc-audio-controls";
-
-  // Create the CC button
-  var ccButton = document.querySelector('.vjs-subs-caps-button');
-  ccButton.className = "vjs-control vjs-button vjs-settings-control"; // Change class to vjs-settings-control
-
-  // Create the Audio Track button
-  var audioButton = document.querySelector('.vjs-audio-button');
-  audioButton.className = "vjs-control vjs-button vjs-settings-control"; // Change class to vjs-settings-control
-
-  // Append the CC and Audio Track buttons to the ccAudioControls div
-  ccAudioControls.appendChild(ccButton);
-  ccAudioControls.appendChild(audioButton);
-
-  // Append the cog icon and ccAudioControls to the settingsMenu
-  settingsMenu.appendChild(cogIcon);
-  settingsMenu.appendChild(ccAudioControls);
-
-  // Append the settingsMenu to the control bar
-  var controlBar = player.controlBar.el();
-  controlBar.appendChild(settingsMenu);
-
-  // Add click event listener for the cog icon to toggle the settings menu
-  cogIcon.addEventListener("click", function() {
-    if (ccAudioControls.style.display === "block") {
-      ccAudioControls.style.display = "none";
+  // You can add event listeners to the new Settings button and menu items to handle their functionality
+  settingsButton.addEventListener('click', function () {
+    // Show/hide the Settings menu as needed
+    if (settingsMenu.style.display === 'none') {
+      settingsMenu.style.display = 'block';
     } else {
-      ccAudioControls.style.display = "block";
+      settingsMenu.style.display = 'none';
     }
   });
+
+  captionsSettingsItem.addEventListener('click', function () {
+  // Implement captions settings behavior
+});
+
+audioSettingsItem.addEventListener('click', function () {
+  // Implement audio settings behavior
+});
+
+settingsButton.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowDown') {
+    // Navigate to the next menu item
+  } else if (e.key === 'ArrowUp') {
+    // Navigate to the previous menu item
+  } else if (e.key === 'Enter') {
+    // Select the currently focused menu item
+  } else if (e.key === 'Esc') {
+    // Close the menu
+  }
 });
