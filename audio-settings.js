da1a2c55e1a00e936a00aa6bc3fc4fc5d0c70534
Sharon C <99379001+schris48@users.noreply.github.com>
2023-10-26 14:03:33 -0400

    Update audio-settings.js

--- audio-settings.js
+++ audio-settings.js
@@ -1,66 +1,56 @@
-// Wait for the Brightcove player to be ready
-videojs.getPlayer('JQrljZgnO6').ready(function () {
-  var player = this;
+videojs.registerPlugin('settingsPlugin', function () {
+  const player = this;
 
-  // Create a new Settings button
-  var settingsButton = document.createElement('button');
-  settingsButton.className = 'vjs-settings-button vjs-menu-button vjs-menu-button-popup vjs-control vjs-button';
-  settingsButton.type = 'button';
-  settingsButton.innerHTML = '<span class="vjs-icon-placeholder" aria-hidden="true"></span><span class="vjs-control-text" aria-live="polite">Settings</span>';
-  
-  // Create a new Settings menu
-  var settingsMenu = document.createElement('div');
-  settingsMenu.className = 'vjs-menu';
-  
-  // Create and add items to the Settings menu
-  var captionsSettingsItem = document.createElement('li');
-  captionsSettingsItem.className = 'vjs-menu-item vjs-texttrack-settings';
-  captionsSettingsItem.innerHTML = '<span class="vjs-menu-item-text">captions settings</span><span class="vjs-control-text" aria-live="polite">, opens captions settings dialog</span>';
-  
-  var audioSettingsItem = document.createElement('li');
-  audioSettingsItem.className = 'vjs-menu-item vjs-texttrack-settings';
-  audioSettingsItem.innerHTML = '<span class="vjs-menu-item-text">audio settings</span><span class="vjs-control-text" aria-live="polite">, opens audio settings dialog</span>';
-  
-  settingsMenu.appendChild(captionsSettingsItem);
-  settingsMenu.appendChild(audioSettingsItem);
-  
-  // Add the Settings button and menu to the control bar
-  var controlBar = player.getChild('ControlBar');
-  controlBar.addChild(settingsButton);
-  controlBar.el().appendChild(settingsMenu);
-
-  // Hide the original Captions and Audio Track buttons
-  var captionsButton = player.controlBar.getChild('ClosedCaptionButton');
-  var audioButton = player.controlBar.getChild('AudioTrackButton');
-  captionsButton.hide();
-  audioButton.hide();
-
-  // You can add event listeners to the new Settings button and menu items to handle their functionality
-  settingsButton.addEventListener('click', function () {
-    // Show/hide the Settings menu as needed
-    if (settingsMenu.style.display === 'none') {
-      settingsMenu.style.display = 'block';
-    } else {
-      settingsMenu.style.display = 'none';
-    }
+  // Reference to the Settings button
+  const settingsButton = player.controlBar.addChild('button', {
+    text: 'Settings', // Button text (or you can add an icon here)
+    el: videojs.createEl('button', {
+      className: 'vjs-settings-button',
+      onclick: function () {
+        // Toggle the menu on button click
+        if (menuEl.style.display === 'none') {
+          menuEl.style.display = 'block';
+        } else {
+          menuEl.style.display = 'none';
+        }
+      },
+    }),
   });
 
-  captionsSettingsItem.addEventListener('click', function () {
-  // Implement captions settings behavior
+  // Create the menu
+  const menuEl = videojs.createEl('div', {
+    className: 'vjs-menu',
+  });
+  settingsButton.el().appendChild(menuEl);
+
+  // Add menu items (Captions and Audio Track)
+  const captionsItem = videojs.createEl('li', {
+    className: 'vjs-menu-item',
+    textContent: 'Captions',
+  });
+  menuEl.appendChild(captionsItem);
+
+  const audioTrackItem = videojs.createEl('li', {
+    className: 'vjs-menu-item',
+    textContent: 'Audio Track',
+  });
+  menuEl.appendChild(audioTrackItem);
+
+  // Hide the menu by default
+  menuEl.style.display = 'none';
+
+  // Handle menu item clicks
+  captionsItem.addEventListener('click', function () {
+    // Add your logic to handle captions
+    // Example: player.toggleCaptions();
+  });
+
+  audioTrackItem.addEventListener('click', function () {
+    // Add your logic to handle audio tracks
+    // Example: player.switchAudioTrack();
+  });
 });
 
-audioSettingsItem.addEventListener('click', function () {
-  // Implement audio settings behavior
-});
-
-settingsButton.addEventListener('keydown', function (e) {
-  if (e.key === 'ArrowDown') {
-    // Navigate to the next menu item
-  } else if (e.key === 'ArrowUp') {
-    // Navigate to the previous menu item
-  } else if (e.key === 'Enter') {
-    // Select the currently focused menu item
-  } else if (e.key === 'Esc') {
-    // Close the menu
-  }
-});
+// Initialize the plugin on your Brightcove video player
+const player = videojs('your-video-player-id');
+player.settingsPlugin(); // Call your custom plugin
