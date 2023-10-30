7f975b38c81feff641f585e326d495cd38fee2e0
Sharon C <99379001+schris48@users.noreply.github.com>
2023-10-30 14:38:55 -0400

    Update sc_customPlayerSettings.js

--- sc_customPlayerSettings.js
+++ sc_customPlayerSettings.js
@@ -1,22 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const settingsButton = document.getElementById("settings-button");
-    const settingsMenu = document.getElementById("settings-menu");
+videojs.registerPlugin('customSettingsButton', function() {
+  const player = this;
+  const settingsButton = document.createElement("button");
+  settingsButton.className = "vjs-control vjs-button vjs-settings-button";
+  settingsButton.innerHTML = "<span class='cog-icon'></span> Settings";
 
-    settingsButton.addEventListener("click", function() {
-        settingsMenu.style.display = settingsMenu.style.display === "block" ? "none" : "block";
-    });
+  // Create the settings menu
+  const settingsMenu = document.createElement("div");
+  settingsMenu.className = "settings-menu";
+  settingsMenu.style.display = "none";
+  
+  const captionsOption = document.createElement("div");
+  captionsOption.className = "menu-item";
+  captionsOption.innerText = "Captions";
+  
+  const audioTrackOption = document.createElement("div");
+  audioTrackOption.className = "menu-item";
+  audioTrackOption.innerText = "Audio Track";
+  
+  settingsMenu.appendChild(captionsOption);
+  settingsMenu.appendChild(audioTrackOption);
 
-    const captionsOption = document.getElementById("captions");
-    const audioTrackOption = document.getElementById("audio-track");
+  // Insert the button and menu into the control bar
+  const controlBar = player.$(".vjs-control-bar");
+  const insertBeforeNode = player.$(".vjs-volume-panel");
 
-    // Add functionality for the "Captions" and "Audio Track" options here
-    captionsOption.addEventListener("click", function() {
-        // Add your logic for captions here
-        console.log("Captions clicked");
-    });
+  controlBar.insertBefore(settingsButton, insertBeforeNode);
+  controlBar.appendChild(settingsMenu);
 
-    audioTrackOption.addEventListener("click", function() {
-        // Add your logic for audio track here
-        console.log("Audio Track clicked");
-    });
+  settingsButton.addEventListener("click", function() {
+    settingsMenu.style.display = settingsMenu.style.display === "block" ? "none" : "block";
+  });
+
+  // Add functionality for the "Captions" and "Audio Track" options here
+  captionsOption.addEventListener("click", function() {
+    // Add your logic for captions here
+    console.log("Captions clicked");
+  });
+
+  audioTrackOption.addEventListener("click", function() {
+    // Add your logic for audio track here
+    console.log("Audio Track clicked");
+  });
 });
