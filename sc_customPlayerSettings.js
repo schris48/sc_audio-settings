52216e345e7d48d3e82a3d6661add6bd543d4942
Sharon C <99379001+schris48@users.noreply.github.com>
2023-10-30 12:02:01 -0400

    Update sc_customPlayerSettings.js

--- sc_customPlayerSettings.js
+++ sc_customPlayerSettings.js
@@ -1,7 +1,22 @@
-videojs.registerPlugin('playerSettings', function(options) {
-    var player = this,
-    overlay = document.createElement('p');
-    overlay.className = 'vjs-overlay';
-    overlay.innerHTML = options.overlayText;
-    player.el().appendChild(overlay);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    const settingsButton = document.getElementById("settings-button");
+    const settingsMenu = document.getElementById("settings-menu");
+
+    settingsButton.addEventListener("click", function() {
+        settingsMenu.style.display = settingsMenu.style.display === "block" ? "none" : "block";
+    });
+
+    const captionsOption = document.getElementById("captions");
+    const audioTrackOption = document.getElementById("audio-track");
+
+    // Add functionality for the "Captions" and "Audio Track" options here
+    captionsOption.addEventListener("click", function() {
+        // Add your logic for captions here
+        console.log("Captions clicked");
+    });
+
+    audioTrackOption.addEventListener("click", function() {
+        // Add your logic for audio track here
+        console.log("Audio Track clicked");
+    });
+});
